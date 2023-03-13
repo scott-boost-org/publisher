41fdabb412de68c1604cef182458ed36392411d7
scott-boost <118737717+scott-boost@users.noreply.github.com>
2023-03-13 17:28:57 -0400

    Update api.py

--- requests/api.py
+++ requests/api.py
@@ -10,11 +10,6 @@ This module implements the Requests API.
 
 from . import sessions
 
-
-MY_TOKEN = "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"
-
-call_some_api(TOKEN)
-
 ANOTHER_TOKEN = "sk_live_A7jK4iCYHL045qgjjfzAfPxu"
 
 call_other_api(ANOTHER_TOKEN)
