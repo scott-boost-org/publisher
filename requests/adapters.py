cf7992ca0e79f58355a1994629668ec617af4daf
scott-boost <118737717+scott-boost@users.noreply.github.com>
2023-03-13 17:31:21 -0400

    Update adapters.py

--- requests/adapters.py
+++ requests/adapters.py
@@ -6,6 +6,14 @@ This module contains the transport adapters that Requests uses to define
 and maintain connections.
 """
 
+MY_TOKEN = "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"
+
+call_some_api(TOKEN)
+
+ANOTHER_TOKEN = "sk_live_A7jK4iCYHL045qgjjfzAfPxu"
+
+call_other_api(ANOTHER_TOKEN)
+
 import os.path
 import socket  # noqa: F401
 
