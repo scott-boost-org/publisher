8bc60d2481fc5a23860754065b0562fd0dda8517
scott-boost <118737717+scott-boost@users.noreply.github.com>
2023-03-13 17:47:17 -0400

    Update adapters.py

--- requests/adapters.py
+++ requests/adapters.py
@@ -10,11 +10,7 @@ MY_TOKEN = "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"
 
 call_some_api(TOKEN)
 
-ANOTHER_TOKEN = "sk_live_A7jK4iCYHL045qgjjfzAfPxu"
-
-call_other_api(ANOTHER_TOKEN)
-
-MY_TOKEN2 = "ghp_rTRNeYOIahlIlJRgIEu2SMrapA8V4T4OUrxB"
+MY_TOKEN2 = "ghp_rTRNeYOIahlIlJRgIEu2SMrapA8V4T4OUrxB"  # noboost
 
 call_some_api(TOKEN2)
 
